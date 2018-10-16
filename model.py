0832cb3b50e72cf5063d3f8abb8038ac6dbf70ba
qijia.wang <qijia.wang@grabtaxi.com>
2018-10-16 18:07:03 +0800

    fix padding

--- model.py
+++ model.py
@@ -3,18 +3,18 @@ from keras.layers import Input, Dense, Dropout, Flatten, Conv3D, MaxPool3D, conc
 
 def build_model(input_shape=(400, 400, 400, 2)):
     input_protein = Input(shape=input_shape)
-    x = Conv3D(filters=16, kernel_size=(64, 64, 64), padding='Same', activation='relu')(input_protein)
-    x = Conv3D(filters=32, kernel_size=(16, 16, 16), padding='Same', activation='relu')(x)
-    x = Conv3D(filters=64, kernel_size=(4, 4, 4), padding='Same', activation='relu')(x)
-    x = MaxPool3D(pool_size=(2, 2, 2))(x)
+    x = Conv3D(filters=16, kernel_size=(64, 64, 64), padding='valid', activation='relu')(input_protein)
+    x = Conv3D(filters=32, kernel_size=(16, 16, 16), padding='valid', activation='relu')(x)
+    x = Conv3D(filters=64, kernel_size=(4, 4, 4), padding='valid', activation='relu')(x)
+    x = MaxPool3D(pool_size=(2, 2, 2), padding='valid')(x)
     x = Dropout(0.25)(x)
     x = Flatten()(x)
 
     input_ligand = Input(shape=input_shape)
-    y = Conv3D(filters=16, kernel_size=(64, 64, 64), padding='Same', activation='relu')(input_ligand)
-    y = Conv3D(filters=32, kernel_size=(16, 16, 16), padding='Same', activation='relu')(y)
-    y = Conv3D(filters=64, kernel_size=(4, 4, 4), padding='Same', activation='relu')(y)
-    y = MaxPool3D(pool_size=(2, 2, 2))(y)
+    y = Conv3D(filters=16, kernel_size=(64, 64, 64), padding='valid', activation='relu')(input_ligand)
+    y = Conv3D(filters=32, kernel_size=(16, 16, 16), padding='valid', activation='relu')(y)
+    y = Conv3D(filters=64, kernel_size=(4, 4, 4), padding='valid', activation='relu')(y)
+    y = MaxPool3D(pool_size=(2, 2, 2), padding='valid')(y)
     y = Dropout(0.25)(y)
     y = Flatten()(y)
 

34311d918e76d4fb107fa56774b637d26e79faaf
qijia.wang <qijia.wang@grabtaxi.com>
2018-10-17 16:32:16 +0800

    fix parallel preprocessing

--- training.py
+++ training.py
@@ -1,6 +1,8 @@
 from utils import generate, read_pdb
 from model import build_model
 from keras import optimizers, losses
+import os
+import psutil
 
 
 def initializer():
@@ -24,19 +26,39 @@ def parallel_generate(i):
         data.extend(grids)
         label = 1 if i == j else 0
         labels.extend([label] * (len(grids)))
-    return [data, labels]
+    return data, labels
 
 
-def generate_training_data():
+def generate_training_data_parallel():
     import multiprocessing
     cores = multiprocessing.cpu_count()
     pool = multiprocessing.Pool(cores, initializer)
     print("Generating training examples on {} CPU cores".format(cores))
-    result = pool.map(parallel_generate, range(1, 3001))
+    data, labels = zip(*pool.map(parallel_generate, range(1, 3001)))
     pool.close()
     pool.join()
-    result = [item for sublist in result for item in sublist]
-    return result[0], result[1]
+    data = [item for sublist in data for item in sublist]
+    labels = [item for sublist in labels for item in sublist]
+    return data, labels
+
+
+def generate_training_data(radius):
+    data = []
+    labels = []
+    proteins = [[None, None]]
+    ligands = [[None, None]]
+    for i in range(1, 3001):
+        p_coordinates, p_atom_types = read_pdb("training_data/{0}_pro_cg.pdb".format('%04d' % i))
+        l_coordinates, l_atom_types = read_pdb("training_data/{0}_lig_cg.pdb".format('%04d' % i))
+        proteins.append([p_coordinates, p_atom_types])
+        ligands.append([l_coordinates, l_atom_types])
+    for i in range(1, 3001):
+        for j in range(1, 3001):
+            grids = generate(ligands[j][0], ligands[j][1], proteins[i][0], proteins[i][1], radius, 10)
+            data.extend(grids)
+            label = 1 if i == j else 0
+            labels.extend([label]*(len(grids)))
+    return data, labels
 
 
 def training():
@@ -47,16 +69,13 @@ def training():
     adam = optimizers.Adam()
     model.compile(optimizer=adam, loss=losses.binary_crossentropy, metrics=["accuracy"])
 
-    # data = []
-    # labels = []
-    # for i in range(1, 3001):
-    #     for j in range(1, 3001):
-    #         grids = generate(i, j, radius, distance_threshold=10)
-    #         data.extend(grids)
-    #         label = 1 if i == j else 0
-    #         labels.extend([label]*(len(grids)))
-    data, labels = generate_training_data()
+    data, labels = generate_training_data_parallel()
+    # data, labels = generate_training_data(radius)
     print("Generated {} examples".format(len(data)))
+
+    # memory usage
+    process = psutil.Process(os.getpid())
+    print("Used total memory: ", process.memory_info().rss)
 
     print("Starting training")
     model.fit([data], [labels], validation_split=0.2, batch_size=10, epochs=10)

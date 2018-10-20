fd718476436c4cae8db91fffe82bc22887fd5304
qijia.wang <qijia.wang@grabtaxi.com>
2018-10-20 17:57:09 +0800

    Tune model

--- model.py
+++ model.py
@@ -4,23 +4,23 @@ from keras.layers import Input, Dense, Dropout, Flatten, Conv3D, MaxPool3D, conc
 
 def build_model(input_shape=(21, 21, 21, 3)):
     input_grid = Input(shape=input_shape)
-    x = Conv3D(filters=16, kernel_size=(8, 8, 8), padding='valid', activation='relu')(input_grid)
-    x = Conv3D(filters=24, kernel_size=(4, 4, 4), padding='valid', activation='relu')(x)
+    x = Conv3D(filters=64, kernel_size=(16, 16, 16), padding='valid', activation='relu')(input_grid)
+    x = Conv3D(filters=128, kernel_size=(8, 8, 8), padding='valid', activation='relu')(x)
     x = MaxPool3D(pool_size=(2, 2, 2), padding='valid')(x)
     x = Dropout(0.25)(x)
 
-    x = Conv3D(filters=32, kernel_size=(4, 4, 4), padding='valid', activation='relu')(x)
-    x = Conv3D(filters=64, kernel_size=(2, 2, 2), padding='valid', activation='relu')(x)
+    x = Conv3D(filters=256, kernel_size=(8, 8, 8), padding='valid', activation='relu')(x)
+    x = Conv3D(filters=256, kernel_size=(4, 4, 4), padding='valid', activation='relu')(x)
     x = MaxPool3D(pool_size=(2, 2, 2), padding='valid')(x)
     x = Dropout(0.25)(x)
     
     x = Flatten()(x)
 
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.5)(x)
     x = Dense(512, activation='relu')(x)
     x = Dropout(0.5)(x)
     x = Dense(256, activation='relu')(x)
-    x = Dropout(0.5)(x)
-    x = Dense(128, activation='relu')(x)
     x = Dropout(0.5)(x)
 
     out = Dense(1, activation="sigmoid")(x)

49cafb1dc90776fd54dcea0db9eaf39f25644fa5
qijia.wang <qijia.wang@grabtaxi.com>
2018-10-25 14:57:31 +0800

    fix

--- training.py
+++ training.py
@@ -182,7 +182,7 @@ if __name__ == '__main__':
     RADIUS = 10
     DISTANCE_THRESHOLD = 10
     NEGATIVE_EXAMPLE = 2
-    TEST_RANGE = 200
+    TEST_RANGE = 825
     VALIDATION_SPLIT = 0.2
     # train_atom()
     # evaluate("v1.h5")
